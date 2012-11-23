f5794fc8b56ad91d7772355c6144149e7b9c659c
jeelukan <jeelukan@gmail.com>
2012-11-23 12:33:29 +0800

    1.修改欢迎页面，添加代码的注释。 2.新建Home页做为功能的导航页面。

---
+++ com.jeelu.android.app.socketplus/src/com/jeelu/android/app/socketplus/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.jeelu.android.app.socketplus;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+
+public class HomeActivity extends Activity
+{
+	@Override
+	public void onCreate(final Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+
+		final Resources resources = getResources();
+		final Window window = getWindow();
+		final Drawable drawable = resources.getDrawable(R.drawable.mainColor);
+		window.setBackgroundDrawable(drawable);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_home, menu);
+		return true;
+	}
+}
--- com.jeelu.android.app.socketplus/src/com/jeelu/android/app/socketplus/WelcomeActivity.java
+++ com.jeelu.android.app.socketplus/src/com/jeelu/android/app/socketplus/WelcomeActivity.java
@@ -12,19 +12,28 @@ public class WelcomeActivity extends Activity
 	@Override
 	public void onCreate(final Bundle savedInstanceState)
 	{
+		// 欢迎页。设置为全屏显示。
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+
+		// 停留4秒钟，以保证相关的视觉信息的传递。4秒钟后跳转到Home页。
 		final Handler x = new Handler();
 		x.postDelayed(new Splashhandler(), 4000);
 	}
 
+	/**
+	 * 欢迎页的动作处理
+	 * 
+	 * @author jeelukan@gmail.com
+	 */
 	class Splashhandler implements Runnable
 	{
 		public void run()
 		{
-			startActivity(new Intent(getApplication(), SocketClientActivity.class));
+			startActivity(new Intent(getApplication(), HomeActivity.class));
 			finish();
 		}
 	}
