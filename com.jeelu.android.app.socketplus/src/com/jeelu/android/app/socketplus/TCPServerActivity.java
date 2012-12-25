12e68ce341a75625eadec7077ca21e6f98507c1e
jeelukan <jeelukan@gmail.com>
2012-12-26 00:49:39 +0800

    SocketServer可用

--- com.jeelu.android.app.socketplus/src/com/jeelu/android/app/socketplus/TCPServerActivity.java
+++ com.jeelu.android.app.socketplus/src/com/jeelu/android/app/socketplus/TCPServerActivity.java
@@ -1,5 +1,12 @@
 package com.jeelu.android.app.socketplus;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.xsocket.connection.Server;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,14 +15,13 @@ import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.jeelu.android.Net.AblySocketServer;
 import com.jeelu.android.Net.NetUtil;
 
 public class TCPServerActivity extends Activity
 {
-	AblySocketServer _SocketServer;
+	Server _SocketServer;
 
-	TextView _LogView;
+	TextView _LogPanel;
 	LinearLayout _ClientByListenListView;
 	TextView _ServerIpTextView;
 
@@ -27,7 +33,7 @@ public class TCPServerActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tcpserver);
 
-		_LogView = (TextView) findViewById(R.id.textview_tcpserver_logpanel);
+		_LogPanel = (TextView) findViewById(R.id.textview_tcpserver_logpanel);
 		_ClientByListenListView = (LinearLayout) findViewById(R.id.linear_clientByListen_iplist);
 		_ServerIpTextView = (TextView) findViewById(R.id.textview_serverinfo_ipaddress);
 
@@ -37,48 +43,70 @@ public class TCPServerActivity extends Activity
 		}
 		catch (Exception e)
 		{
-			Log.w("myapp", e);
+			Log.w("app", e);
 			_ServerIpTextView.setText("127.0.0.1");
 		}
-
-		final Handler handler = new Handler()
-		{
-			@Override
-			public void handleMessage(final Message msg)
-			{
-				_LogView.setText((String) msg.obj);
-			}
-		};
-		creatSocketServer(handler);
+		creatSocketServer(new TcpServerUIHandler());
 	}
 
-	private void creatSocketServer(final Handler handler)
+	private void creatSocketServer(final Handler uiHandler)
 	{
 		Runnable runnable = new Runnable()
 		{
 			public void run()
 			{
-				_SocketServer = new AblySocketServer();
 				try
 				{
-					_SocketServer.startServer(handler);
+					SocketDataHandler processor = new SocketDataHandler();
+					processor.setUIHandler(uiHandler);
+					_SocketServer = new Server(9009, processor);
+					_SocketServer.run();
 				}
-				catch (Exception e)
+				catch (Exception ex)
 				{
-					Log.e("Socket", e.getMessage());
+					Log.e("app", ex.getMessage());
 				}
 			}
 		};
+
 		_Thread = new Thread(runnable);
-		_Thread.setName("AblySocketServer");
 		_Thread.start();
 	}
 
 	@Override
 	protected void onStop()
 	{
-		_SocketServer.stopServer();
+		_SocketServer.close();
 		_Thread.stop();
 		super.onStop();
+	}
+
+	@SuppressLint("HandlerLeak")
+	class TcpServerUIHandler extends Handler
+	{
+		@Override
+		public void handleMessage(final Message msg)
+		{
+			super.handleMessage(msg);
+			switch (msg.what)
+			{
+				case 0:// 数据
+					Date now = new Date();
+					SimpleDateFormat format = (SimpleDateFormat) DateFormat.getTimeInstance();
+					format.applyPattern("mm:ss:SSS");
+					StringBuffer buffer = new StringBuffer(_LogPanel.getText());
+					buffer.insert(0, "\n");
+					buffer.insert(0, msg.obj);
+					buffer.insert(0, "\n");
+					buffer.insert(0, format.format(now));
+					_LogPanel.setText(buffer.toString());
+					break;
+				case 1:
+				case 2:
+				case -1:
+				default:
+					break;
+			}
+		}
 	}
 }
